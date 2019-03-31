512650c747579bb9f9e6727895b068bf67d87226
jcreddy@gmail.com <ADMIN@DESKTOP-V7N4MMG>
2019-03-31 22:31:35 +0530

    i added lombok jars and bo class with properties

---
+++ SpringMvcPratice/src/main/java/com/jc/bo/YsrcpMebershipBo.java
@@ -0,0 +1,15 @@
+package com.jc.bo;
+
+import lombok.Data;
+
+@Data
+public class YsrcpMebershipBo {
+	int mid;
+	private String mName;
+	private String Village;
+	private String constitenucey;
+	private String mandal;
+	private String district;
+	private long mobile;
+
+}
